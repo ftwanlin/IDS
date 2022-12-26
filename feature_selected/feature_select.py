8bd7bb77d1c9101220f030a05157a8c311c9147d
ftwanlin <ftwanlin@gmail.com>
2022-12-26 15:58:44 +0700

    update

---
+++ feature_selected/feature_select.py
@@ -0,0 +1,425 @@
+#https://github.com/Deepthi10/Intrusion-Detection-using-Machine-Learning-on-NSL--KDD-dataset/blob/master/IDS.ipynb
+
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+from sklearn import preprocessing
+from sklearn.feature_selection import RFE
+from sklearn.tree import DecisionTreeClassifier
+import warnings
+warnings.filterwarnings("ignore")
+
+DATA_DIR = os.path.join(os.path.abspath('..'), "data")
+
+# dataset
+dataset_train=pd.read_csv('data\KDDTrain+.csv')
+dataset_test=pd.read_csv('data\KDDTest+.csv')
+
+# tên cột trong train và test
+col_names = ["duration","protocol_type","service","flag","src_bytes",
+    "dst_bytes","land","wrong_fragment","urgent","hot","num_failed_logins",
+    "logged_in","num_compromised","root_shell","su_attempted","num_root",
+    "num_file_creations","num_shells","num_access_files","num_outbound_cmds",
+    "is_host_login","is_guest_login","count","srv_count","serror_rate",
+    "srv_serror_rate","rerror_rate","srv_rerror_rate","same_srv_rate",
+    "diff_srv_rate","srv_diff_host_rate","dst_host_count","dst_host_srv_count",
+    "dst_host_same_srv_rate","dst_host_diff_srv_rate","dst_host_same_src_port_rate",
+    "dst_host_srv_diff_host_rate","dst_host_serror_rate","dst_host_srv_serror_rate",
+    "dst_host_rerror_rate","dst_host_srv_rerror_rate","label"]
+
+
+
+# gán tên cột
+dataset_train = pd.read_csv("data\KDDTrain+.csv", header=None, names = col_names)
+dataset_test = pd.read_csv("data\KDDTest+.csv", header=None, names = col_names)
+
+
+
+# insert code to get a list of categorical columns into a variable, categorical_columns
+categorical_columns=['protocol_type', 'service', 'flag'] 
+ # Get the categorical values into a 2D numpy array
+dataset_train_categorical_values = dataset_train[categorical_columns]
+dataset_test_categorical_values = dataset_test[categorical_columns]
+
+# print(dataset_train_categorical_values.head())
+
+# protocol type
+unique_protocol=sorted(dataset_train.protocol_type.unique())
+string1 = 'Protocol_type_'
+unique_protocol2=[string1 + x for x in unique_protocol]
+# service
+unique_service=sorted(dataset_train.service.unique())
+string2 = 'service_'
+unique_service2=[string2 + x for x in unique_service]
+# flag
+unique_flag=sorted(dataset_train.flag.unique())
+string3 = 'flag_'
+unique_flag2=[string3 + x for x in unique_flag]
+# put together
+dumcols=unique_protocol2 + unique_service2 + unique_flag2
+# print(len(dumcols))
+
+#do same for test set
+unique_service_test=sorted(dataset_test.service.unique())
+unique_service2_test=[string2 + x for x in unique_service_test]
+testdumcols=unique_protocol2 + unique_service2_test + unique_flag2
+
+#Transform categorical features into numbers using LabelEncoder()
+dataset_train_categorical_values_enc=dataset_train_categorical_values.apply(LabelEncoder().fit_transform)
+# print(dataset_train_categorical_values_enc.head())
+# test set
+dataset_test_categorical_values_enc=dataset_test_categorical_values.apply(LabelEncoder().fit_transform)
+
+#One-Hot-Encoding
+enc = OneHotEncoder()
+dataset_train_categorical_values_encenc = enc.fit_transform(dataset_train_categorical_values_enc)
+dataset_train_cat_data = pd.DataFrame(dataset_train_categorical_values_encenc.toarray(),columns=dumcols)
+# test set
+dataset_test_categorical_values_encenc = enc.fit_transform(dataset_test_categorical_values_enc)
+dataset_test_cat_data = pd.DataFrame(dataset_test_categorical_values_encenc.toarray(),columns=testdumcols)
+
+#add 6 missing categories from train set to test set
+trainservice=dataset_train['service'].tolist()
+testservice= dataset_test['service'].tolist()
+difference=list(set(trainservice) - set(testservice))
+string = 'service_'
+difference=[string + x for x in difference]
+# print(difference)
+for col in difference:
+    dataset_test_cat_data[col] = 0
+# print(dataset_train_cat_data.shape)
+# print(dataset_test_cat_data.shape)
+
+# Join encoded categorical dataframe with the non-categorical dataframe
+#Join encoded categorical dataframe with the non-categorical dataframe
+newdf=dataset_train.join(dataset_train_cat_data)
+newdf.drop('flag', axis=1, inplace=True)
+newdf.drop('protocol_type', axis=1, inplace=True)
+newdf.drop('service', axis=1, inplace=True)
+# test data
+newdf_test=dataset_test.join(dataset_test_cat_data)
+newdf_test.drop('flag', axis=1, inplace=True)
+newdf_test.drop('protocol_type', axis=1, inplace=True)
+newdf_test.drop('service', axis=1, inplace=True)
+# print(newdf.shape)
+# print(newdf_test.shape)
+
+# Split Dataset into 4 datasets for every attack category
+# Rename every attack label: 0=normal, 1=DoS, 2=Probe, 3=R2L and 4=U2R.
+# Replace labels column with new labels column
+# Make new datasets
+# take label column
+labeldf=newdf['label']
+labeldf_test=newdf_test['label']
+# change the label column
+newlabeldf=labeldf.replace({ 'normal' : 0, 'neptune' : 1 ,'back': 1, 'land': 1, 'pod': 1, 'smurf': 1, 'teardrop': 1,'mailbomb': 1, 'apache2': 1, 'processtable': 1, 'udpstorm': 1, 'worm': 1,
+                           'ipsweep' : 2,'nmap' : 2,'portsweep' : 2,'satan' : 2,'mscan' : 2,'saint' : 2
+                           ,'ftp_write': 3,'guess_passwd': 3,'imap': 3,'multihop': 3,'phf': 3,'spy': 3,'warezclient': 3,'warezmaster': 3,'sendmail': 3,'named': 3,'snmpgetattack': 3,'snmpguess': 3,'xlock': 3,'xsnoop': 3,'httptunnel': 3,
+                           'buffer_overflow': 4,'loadmodule': 4,'perl': 4,'rootkit': 4,'ps': 4,'sqlattack': 4,'xterm': 4})
+newlabeldf_test=labeldf_test.replace({ 'normal' : 0, 'neptune' : 1 ,'back': 1, 'land': 1, 'pod': 1, 'smurf': 1, 'teardrop': 1,'mailbomb': 1, 'apache2': 1, 'processtable': 1, 'udpstorm': 1, 'worm': 1,
+                           'ipsweep' : 2,'nmap' : 2,'portsweep' : 2,'satan' : 2,'mscan' : 2,'saint' : 2
+                           ,'ftp_write': 3,'guess_passwd': 3,'imap': 3,'multihop': 3,'phf': 3,'spy': 3,'warezclient': 3,'warezmaster': 3,'sendmail': 3,'named': 3,'snmpgetattack': 3,'snmpguess': 3,'xlock': 3,'xsnoop': 3,'httptunnel': 3,
+                           'buffer_overflow': 4,'loadmodule': 4,'perl': 4,'rootkit': 4,'ps': 4,'sqlattack': 4,'xterm': 4})
+# put the new label column back
+newdf['label'] = newlabeldf
+newdf_test['label'] = newlabeldf_test
+# print(newdf['label'].head())
+to_drop_DoS = [2,3,4]
+to_drop_Probe = [1,3,4]
+to_drop_R2L = [1,2,4]
+to_drop_U2R = [1,2,3]
+DoS_df=newdf[~newdf['label'].isin(to_drop_DoS)];
+Probe_df=newdf[~newdf['label'].isin(to_drop_Probe)];
+R2L_df=newdf[~newdf['label'].isin(to_drop_R2L)];
+U2R_df=newdf[~newdf['label'].isin(to_drop_U2R)];
+#test
+DoS_df_test=newdf_test[~newdf_test['label'].isin(to_drop_DoS)];
+Probe_df_test=newdf_test[~newdf_test['label'].isin(to_drop_Probe)];
+R2L_df_test=newdf_test[~newdf_test['label'].isin(to_drop_R2L)];
+U2R_df_test=newdf_test[~newdf_test['label'].isin(to_drop_U2R)];
+
+# feature scaling
+# Split dataframes into X & Y
+# assign X as a dataframe of feautures and Y as a series of outcome variables
+X_DoS = DoS_df.drop('label',1)
+Y_DoS = DoS_df.label
+X_Probe = Probe_df.drop('label',1)
+Y_Probe = Probe_df.label
+X_R2L = R2L_df.drop('label',1)
+Y_R2L = R2L_df.label
+X_U2R = U2R_df.drop('label',1)
+Y_U2R = U2R_df.label
+# test set
+X_DoS_test = DoS_df_test.drop('label',1)
+Y_DoS_test = DoS_df_test.label
+X_Probe_test = Probe_df_test.drop('label',1)
+Y_Probe_test = Probe_df_test.label
+X_R2L_test = R2L_df_test.drop('label',1)
+Y_R2L_test = R2L_df_test.label
+X_U2R_test = U2R_df_test.drop('label',1)
+Y_U2R_test = U2R_df_test.label
+
+# Save a list of feature names for later use (it is the same for every attack category). Column names are dropped at this stage.
+colNames=list(X_DoS)
+colNames_test=list(X_DoS_test)
+
+# Use StandardScaler() to scale the dataframes
+scaler1 = preprocessing.StandardScaler().fit(X_DoS)
+X_DoS=scaler1.transform(X_DoS) 
+scaler2 = preprocessing.StandardScaler().fit(X_Probe)
+X_Probe=scaler2.transform(X_Probe) 
+scaler3 = preprocessing.StandardScaler().fit(X_R2L)
+X_R2L=scaler3.transform(X_R2L) 
+scaler4 = preprocessing.StandardScaler().fit(X_U2R)
+X_U2R=scaler4.transform(X_U2R) 
+# test data
+scaler5 = preprocessing.StandardScaler().fit(X_DoS_test)
+X_DoS_test=scaler5.transform(X_DoS_test) 
+scaler6 = preprocessing.StandardScaler().fit(X_Probe_test)
+X_Probe_test=scaler6.transform(X_Probe_test) 
+scaler7 = preprocessing.StandardScaler().fit(X_R2L_test)
+X_R2L_test=scaler7.transform(X_R2L_test) 
+scaler8 = preprocessing.StandardScaler().fit(X_U2R_test)
+X_U2R_test=scaler8.transform(X_U2R_test)
+
+
+
+# Step 3: Feature Selection:
+# 1. Univariate Feature Selection using ANOVA F-test
+#univariate feature selection with ANOVA F-test. using secondPercentile method, then RFE
+#Scikit-learn exposes feature selection routines as objects that implement the transform method
+#SelectPercentile: removes all but a user-specified highest scoring percentage of features
+#f_classif: ANOVA F-value between label/feature for classification tasks.
+from sklearn.feature_selection import SelectPercentile, f_classif
+np.seterr(divide='ignore', invalid='ignore');
+selector=SelectPercentile(f_classif, percentile=10)
+X_newDoS = selector.fit_transform(X_DoS,Y_DoS)
+
+# Get the features that were selected: DoS
+true=selector.get_support()
+newcolindex_DoS=[i for i, x in enumerate(true) if x]
+newcolname_DoS=list( colNames[i] for i in newcolindex_DoS )
+X_newProbe = selector.fit_transform(X_Probe,Y_Probe)
+
+# Get the features that were selected: Probe
+true=selector.get_support()
+newcolindex_Probe=[i for i, x in enumerate(true) if x]
+newcolname_Probe=list( colNames[i] for i in newcolindex_Probe )
+X_newR2L = selector.fit_transform(X_R2L,Y_R2L)
+
+# Get the features that were selected: R2L
+true=selector.get_support()
+newcolindex_R2L=[i for i, x in enumerate(true) if x]
+newcolname_R2L=list( colNames[i] for i in newcolindex_R2L)
+X_newU2R = selector.fit_transform(X_U2R,Y_U2R)
+
+# Get the features that were selected: U2R
+true=selector.get_support()
+newcolindex_U2R=[i for i, x in enumerate(true) if x]
+newcolname_U2R=list( colNames[i] for i in newcolindex_U2R)
+
+# # 2. Recursive Feature Elimination for feature ranking (Option 1: get importance from previous selected)
+# from sklearn.feature_selection import RFE
+# from sklearn.tree import DecisionTreeClassifier
+# # Create a decision tree classifier. By convention, clf means 'classifier'
+# clf = DecisionTreeClassifier(random_state=0)
+
+# #rank all features, i.e continue the elimination until the last one
+# rfe = RFE(clf, n_features_to_select=1)
+# rfe.fit(X_newDoS, Y_DoS.astype('int'))
+# rfe.fit(X_newProbe, Y_Probe.astype(int))
+# rfe.fit(X_newR2L, Y_R2L.astype(int))
+# rfe.fit(X_newU2R, Y_U2R.astype(int))
+
+# Recursive Feature Elimination, select 13 features each of 122 (Option 2: get 13 best features from 122 from RFE)
+from sklearn.feature_selection import RFE
+clf = DecisionTreeClassifier(random_state=0)
+rfe = RFE(estimator=clf, n_features_to_select=13, step=1)
+# DoS
+rfe.fit(X_DoS, Y_DoS.astype(int))
+X_rfeDoS=rfe.transform(X_DoS)
+true=rfe.support_
+rfecolindex_DoS=[i for i, x in enumerate(true) if x]
+rfecolname_DoS=list(colNames[i] for i in rfecolindex_DoS)
+# Probe
+rfe.fit(X_Probe, Y_Probe.astype(int))
+X_rfeProbe=rfe.transform(X_Probe)
+true=rfe.support_
+rfecolindex_Probe=[i for i, x in enumerate(true) if x]
+rfecolname_Probe=list(colNames[i] for i in rfecolindex_Probe)
+# R2L
+rfe.fit(X_R2L, Y_R2L.astype(int))
+X_rfeR2L=rfe.transform(X_R2L)
+true=rfe.support_
+rfecolindex_R2L=[i for i, x in enumerate(true) if x]
+rfecolname_R2L=list(colNames[i] for i in rfecolindex_R2L)
+# U2R
+rfe.fit(X_U2R, Y_U2R.astype(int))
+X_rfeU2R=rfe.transform(X_U2R)
+true=rfe.support_
+rfecolindex_U2R=[i for i, x in enumerate(true) if x]
+rfecolname_U2R=list(colNames[i] for i in rfecolindex_U2R)
+
+print('Features selected for DoS:',rfecolname_DoS)
+print()
+print('Features selected for Probe:',rfecolname_Probe)
+print()
+print('Features selected for R2L:',rfecolname_R2L)
+print()
+print('Features selected for U2R:',rfecolname_U2R)
+
+# Step 4: Build the model:
+# Classifier is trained for all features and for reduced features, for later comparison.
+# The classifier model itself is stored in the clf variable.
+
+# all features
+clf_DoS=DecisionTreeClassifier(random_state=0)
+clf_Probe=DecisionTreeClassifier(random_state=0)
+clf_R2L=DecisionTreeClassifier(random_state=0)
+clf_U2R=DecisionTreeClassifier(random_state=0)
+clf_DoS.fit(X_DoS, Y_DoS.astype(int))
+clf_Probe.fit(X_Probe, Y_Probe.astype(int))
+clf_R2L.fit(X_R2L, Y_R2L.astype(int))
+clf_U2R.fit(X_U2R, Y_U2R.astype(int))
+
+# selected features
+clf_rfeDoS=DecisionTreeClassifier(random_state=0)
+clf_rfeProbe=DecisionTreeClassifier(random_state=0)
+clf_rfeR2L=DecisionTreeClassifier(random_state=0)
+clf_rfeU2R=DecisionTreeClassifier(random_state=0)
+clf_rfeDoS.fit(X_rfeDoS, Y_DoS.astype(int))
+clf_rfeProbe.fit(X_rfeProbe, Y_Probe.astype(int))
+clf_rfeR2L.fit(X_rfeR2L, Y_R2L.astype(int))
+clf_rfeU2R.fit(X_rfeU2R, Y_U2R.astype(int))
+
+# 5. Prediction & Evaluation (validation):
+# Using all Features for each category
+# Confusion Matrices : DoS
+
+from sklearn.metrics import classification_report
+
+# Apply the classifier we trained to the test data (which it has never seen before)
+clf_DoS.predict(X_DoS_test)
+
+# View the predicted probabilities of the first 10 observations
+clf_DoS.predict_proba(X_DoS_test)[0:10]
+
+Y_DoS_pred=clf_DoS.predict(X_DoS_test)
+# Create confusion matrix
+pd.crosstab(Y_DoS_test, Y_DoS_pred, rownames=['Actual attacks'], colnames=['Predicted attacks'])
+print(classification_report(Y_DoS_test, Y_DoS_pred))
+
+# Confusion Matrices : Probe
+Y_Probe_pred=clf_Probe.predict(X_Probe_test)
+# Create confusion matrix
+pd.crosstab(Y_Probe_test, Y_Probe_pred, rownames=['Actual attacks'], colnames=['Predicted attacks'])
+print(classification_report(Y_Probe_test, Y_Probe_pred))
+
+# Confusion Matrices : R2L
+Y_R2L_pred=clf_R2L.predict(X_R2L_test)
+# Create confusion matrix
+pd.crosstab(Y_R2L_test, Y_R2L_pred, rownames=['Actual attacks'], colnames=['Predicted attacks'])
+print(classification_report(Y_R2L_test, Y_R2L_pred))
+
+# Confusion Matrices : U2R
+Y_U2R_pred=clf_U2R.predict(X_U2R_test)
+# Create confusion matrix
+pd.crosstab(Y_U2R_test, Y_U2R_pred, rownames=['Actual attacks'], colnames=['Predicted attacks'])
+print(classification_report(Y_U2R_test, Y_U2R_pred))
+
+# Cross Validation: Accuracy, Precision, Recall, F-measure
+# DoS
+from sklearn.model_selection import cross_val_score
+from sklearn import metrics
+print("DoS:")
+accuracy = cross_val_score(clf_DoS, X_DoS_test, Y_DoS_test, cv=10, scoring='accuracy')
+print("Accuracy: %0.5f (+/- %0.5f)" % (accuracy.mean(), accuracy.std() * 2))
+precision = cross_val_score(clf_DoS, X_DoS_test, Y_DoS_test, cv=10, scoring='precision')
+print("Precision: %0.5f (+/- %0.5f)" % (precision.mean(), precision.std() * 2))
+recall = cross_val_score(clf_DoS, X_DoS_test, Y_DoS_test, cv=10, scoring='recall')
+print("Recall: %0.5f (+/- %0.5f)" % (recall.mean(), recall.std() * 2))
+f = cross_val_score(clf_DoS, X_DoS_test, Y_DoS_test, cv=10, scoring='f1')
+print("F-measure: %0.5f (+/- %0.5f)" % (f.mean(), f.std() * 2))
+
+# Probe
+print("Probe:")
+accuracy = cross_val_score(clf_Probe, X_Probe_test, Y_Probe_test, cv=10, scoring='accuracy')
+print("Accuracy: %0.5f (+/- %0.5f)" % (accuracy.mean(), accuracy.std() * 2))
+precision = cross_val_score(clf_Probe, X_Probe_test, Y_Probe_test, cv=10, scoring='precision_macro')
+print("Precision: %0.5f (+/- %0.5f)" % (precision.mean(), precision.std() * 2))
+recall = cross_val_score(clf_Probe, X_Probe_test, Y_Probe_test, cv=10, scoring='recall_macro')
+print("Recall: %0.5f (+/- %0.5f)" % (recall.mean(), recall.std() * 2))
+f = cross_val_score(clf_Probe, X_Probe_test, Y_Probe_test, cv=10, scoring='f1_macro')
+print("F-measure: %0.5f (+/- %0.5f)" % (f.mean(), f.std() * 2))
+
+# R2L
+print("R2L")
+accuracy = cross_val_score(clf_R2L, X_R2L_test, Y_R2L_test, cv=10, scoring='accuracy')
+print("Accuracy: %0.5f (+/- %0.5f)" % (accuracy.mean(), accuracy.std() * 2))
+precision = cross_val_score(clf_R2L, X_R2L_test, Y_R2L_test, cv=10, scoring='precision_macro')
+print("Precision: %0.5f (+/- %0.5f)" % (precision.mean(), precision.std() * 2))
+recall = cross_val_score(clf_R2L, X_R2L_test, Y_R2L_test, cv=10, scoring='recall_macro')
+print("Recall: %0.5f (+/- %0.5f)" % (recall.mean(), recall.std() * 2))
+f = cross_val_score(clf_R2L, X_R2L_test, Y_R2L_test, cv=10, scoring='f1_macro')
+print("F-measure: %0.5f (+/- %0.5f)" % (f.mean(), f.std() * 2))
+
+# U2R
+print("U2R")
+accuracy = cross_val_score(clf_U2R, X_U2R_test, Y_U2R_test, cv=10, scoring='accuracy')
+print("Accuracy: %0.5f (+/- %0.5f)" % (accuracy.mean(), accuracy.std() * 2))
+precision = cross_val_score(clf_U2R, X_U2R_test, Y_U2R_test, cv=10, scoring='precision_macro')
+print("Precision: %0.5f (+/- %0.5f)" % (precision.mean(), precision.std() * 2))
+recall = cross_val_score(clf_U2R, X_U2R_test, Y_U2R_test, cv=10, scoring='recall_macro')
+print("Recall: %0.5f (+/- %0.5f)" % (recall.mean(), recall.std() * 2))
+f = cross_val_score(clf_U2R, X_U2R_test, Y_U2R_test, cv=10, scoring='f1_macro')
+print("F-measure: %0.5f (+/- %0.5f)" % (f.mean(), f.std() * 2))
+
+# RFECV for illustration
+print(__doc__)
+
+import matplotlib.pyplot as plt
+from sklearn.feature_selection import RFECV
+from sklearn.model_selection import StratifiedKFold
+# %matplotlib
+
+rfecv_DoS = RFECV(estimator=clf_DoS, step=1, cv=10, scoring='accuracy')
+rfecv_DoS.fit(X_DoS_test, Y_DoS_test)
+# Plot number of features VS. cross-validation scores
+plt.figure()
+plt.xlabel("Number of features selected")
+plt.ylabel("Cross validation score (nb of correct classifications)")
+plt.title('RFECV DoS')
+plt.plot(range(1, len(rfecv_DoS.grid_scores_) + 1), rfecv_DoS.grid_scores_)
+plt.savefig('feature_selected\RFECV DoS.png')
+
+rfecv_Probe = RFECV(estimator=clf_Probe, step=1, cv=10, scoring='accuracy')
+rfecv_Probe.fit(X_Probe_test, Y_Probe_test)
+# Plot number of features VS. cross-validation scores
+plt.figure()
+plt.xlabel("Number of features selected")
+plt.ylabel("Cross validation score (nb of correct classifications)")
+plt.title('RFECV Probe')
+plt.plot(range(1, len(rfecv_Probe.grid_scores_) + 1), rfecv_Probe.grid_scores_)
+plt.savefig('feature_selected\RFECV Probe.png')
+
+rfecv_R2L = RFECV(estimator=clf_R2L, step=1, cv=10, scoring='accuracy')
+rfecv_R2L.fit(X_R2L_test, Y_R2L_test)
+# Plot number of features VS. cross-validation scores
+plt.figure()
+plt.xlabel("Number of features selected")
+plt.ylabel("Cross validation score (nb of correct classifications)")
+plt.title('RFECV R2L')
+plt.plot(range(1, len(rfecv_R2L.grid_scores_) + 1), rfecv_R2L.grid_scores_)
+plt.savefig('feature_selected\RFECV R2L.png')
+
+rfecv_U2R = RFECV(estimator=clf_U2R, step=1, cv=10, scoring='accuracy')
+rfecv_U2R.fit(X_U2R_test, Y_U2R_test)
+# Plot number of features VS. cross-validation scores
+plt.figure()
+plt.xlabel("Number of features selected")
+plt.ylabel("Cross validation score (nb of correct classifications)")
+plt.title('RFECV U2R')
+plt.plot(range(1, len(rfecv_U2R.grid_scores_) + 1), rfecv_U2R.grid_scores_)
+plt.savefig('feature_selected\RFECV U2R.png')
